10dad7c2d0b502c664f03ab15de39fde0a8560fe
Griffin Millender <griffinn.millender@gmail.com>
2013-10-21 20:55:41 -0500

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,12 +4,16 @@ then
     export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
 fi
 
+cd $WORKSPACE
+mkdir -p ../slim
+cd ../slim
+export WORKSPACE="$PWD"
+
 rm -rf scripts
 git clone git://github.com/gmillz/scripts.git
 
 export PATH=${PATH}:~/scripts:~/bin
 
 cd scripts
-export WORKSPACE="$PWD"
 chmod a+x ./build.sh
 exec ./build.sh
