73938efff9134f882b009fd0b3042cb805353a1c
Griffin Millender <griffinn.millender@gmail.com>
2013-10-21 20:07:56 -0500

    updates

--- job.sh
+++ job.sh
@@ -4,10 +4,7 @@ then
     export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
 fi
 
-if [ ! -d "scripts" ]
-then
-    git clone git://github.com/gmillz/scripts.git
-fi
+git clone git://github.com/gmillz/scripts.git
 
 export PATH=${PATH}:~/scripts:~/bin
 
