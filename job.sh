dc7e6d6fef87c0f0bfc36a6acfeeaf7c707fe424
Griffin Millender <griffinn.millender@gmail.com>
2013-10-21 19:50:59 -0500

    Add job.sh

---
+++ job.sh
@@ -0,0 +1,13 @@
+if [ -z "$HOME" ]
+then
+    echo HOME not in environment, guessing...
+    export HOME=$(awk -F: -v v="$USER" '{if ($1==v) print $6}' /etc/passwd)
+fi
+
+if [ ! -d "scripts" ]
+then
+    git clone git://github.com/gmillz/scripts.git
+fi
+
+cd hudson
+exec ./build.sh
