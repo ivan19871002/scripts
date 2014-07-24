0d2ba0053d6d0306271bd109cf0c5a75277fdf31
Griffin Millender <griffinn.millender@gmail.com>
2014-07-24 18:00:24 -0400

    Update backup script

--- backup.sh
+++ backup.sh
@@ -6,7 +6,8 @@ PORT=2212
 DATE=$(date +%Y%m%d)
 BACKUP_FOLDER=/Backups
 BACKUP_FILE="$BACKUP_FOLDER/gmillz-chromebook_backup_$Date.tar.gz"
-BACKUP_LOCATION=/mnt/Media/Backups
+BACKUP=`basename $BACKUP_FILE`
+SERVER_LOCATION=/mnt/Media/Backups
 SHOULD_UPLOAD=false
 
 BACKUP_EXCLUDE=("/proc/*"
@@ -23,7 +24,7 @@ do
     TAR_ARGS="$TAR_ARGS --exclude=$exclude"
 done
 
-echo "sudo tar "$TAR_ARGS""
+sudo tar "$TAR_ARGS"
 
 if sftp -P $PORT $USER@$HOST <<< 'pwd' >/dev/null 2>&1
 then 
@@ -34,8 +35,24 @@ if [ "$SHOULD_UPLOAD" = true ]
 then
     echo "Uploading..."
     echo "
-        cd $BACKUP_LOCATION
-        put $BCACKUP_FILE
+        cd $SERVER_LOCATION
+        put $BACKUP_FILE
         exit
     " | sftp -P $PORT $USER@$HOST
+fi
+
+SERVER_MD5=`ssh $USER@$HOST -p $PORT \"md5sum $SERVER_LOCATION/$BACKUP\" | awk {print $1}`
+LOCAL_MD5=`md5sum $BACKUP_FILE | awk {print $1}`
+
+if [ "$SERVER_MD5" = "$LOCAL_MD5" ]
+then
+    sudo rm -f $BACKUP_FILE
+    echo "Backup complete."
+else
+    FILE=`basename $(ssh $USER@$HOST -p $PORT "ls $TEST")`
+    if [ "$FILE" = "$BACKUP" ]
+    then
+        sudo rm -f $BACKUP_FILE
+        ssh $USER@$HOST -p $PORT "rm -f $SERVER_LOCATION/$BACKUP"
+    fi
 fi
